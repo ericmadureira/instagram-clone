50437b35fbc06ebbd53a17703547c108f2af938b
Eric Madureira <ericmadureira.uesc@gmail.com>
2020-06-02 21:39:19 -0300

    feat: Home page bootstrap
    Bootstraps the Home page component.

---
+++ src/pages/Home.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Home = styled.div`
+  align-items: center;
+  display: flex;
+  flex-direction: column;
+`;
+
+const Container = () => {
+  return (
+    <Home>
+      Home Page
+    </Home>
+  )
+}
+
+export default Container;
