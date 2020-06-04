3783c9acf83d62066a80d1430db1c69460587446
Eric Madureira <ericmadureira.uesc@gmail.com>
2020-06-03 22:43:08 -0300

    feat: minor semantic and lint fixes
    Changes the Layout html tag to the semantic 'main' and turns the
    prop type declaration into a oneliner.

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -3,16 +3,14 @@ import styled from 'styled-components';
 
 import Header from '../components/Header';
 
-const Layout = styled.div`
+const Layout = styled.main`
   align-items: center;
   background-color: #FAFAFA;
   display: flex;
   flex-direction: column;
 `;
 
-type Props = {
-  children: JSX.Element,
-};
+type Props = { children: JSX.Element };
 
 const Container = (
   { children }: Props,
