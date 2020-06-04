40d4813789553a881a08eeb31504d46cd33ad7a7
Eric Madureira <ericmadureira.uesc@gmail.com>
2020-06-03 22:42:19 -0300

    feat: add Feed to Home page
    Adds the Feed component to the Home page.

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import styled from 'styled-components';
 
 import StoriesBar from '../components/StoriesBar';
+import Feed from '../components/Feed';
 
 const Home = styled.div`
   align-items: center;
@@ -13,6 +14,7 @@ const Container = () => {
   return (
     <Home>
       <StoriesBar />
+      <Feed />
     </Home>
   );
 };
